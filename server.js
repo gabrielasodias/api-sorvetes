ea6db00fc5c043bdf4b4a133aceb5a512bcf1407
Gabriela <gabrieladiasofc@gmail.com>
2024-10-16 10:34:17 -0300

    Send data to the database

--- server.js
+++ server.js
@@ -1,20 +1,69 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const sabores = []
+app.post('/sabores', async (req, res) => {
 
-app.post('/sabores', (req, res) => {
-
-    sabores.push(req.body)
+    await prisma.sabores.create({
+        data: {
+            tipoSorvete: req.body.tipoSorvete,
+            sabor: req.body.sabor,
+            quantidade: req.body.quantidade
+        }
+    })
 
     res.status(201).json(req.body)
     
 })
 
-app.get('/sabores', (req, res) => {
+app.get('/sabores', async(req, res) => {
+
+    let sabores = []
+    
+    if (req.query) {
+        sabores = await prisma.sabores.findMany({
+            where: {
+                tipoSorvete: req.query.tipoSorvete,
+                sabor: req.query.sabor
+            }
+        })   
+    } else {
+        sabores = await prisma.sabores.findMany()
+    }
+
     res.status(200).json(sabores)
+})
+
+app.put('/sabores/:id', async (req, res) => {
+
+    await prisma.sabores.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            tipoSorvete: req.body.tipoSorvete,
+            sabor: req.body.sabor,
+            quantidade: req.body.quantidade
+        }
+    })
+
+    res.status(201).json(req.body)
+    
+})
+
+app.delete('/sabores/:id', async (req, res) => {
+
+    await prisma.sabores.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: 'Sabor deletado com sucesso'})
 })
 
 app.listen(3000)
